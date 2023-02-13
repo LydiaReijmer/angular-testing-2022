c398779d8c9634bee186ed150e7bab24cb31fa1d
Lydia Reijmer <lydia.reijmer@cegeka.com>
2023-02-13 17:04:19 +0100

    added step 4

--- angular-test-basics/src/app/app.component.spec.ts
+++ angular-test-basics/src/app/app.component.spec.ts
@@ -1,11 +1,17 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatCheckboxHarness } from '@angular/material/checkbox/testing'
 
 describe('AppComponent', () => {
+  let loader: HarnessLoader;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -16,6 +22,8 @@ describe('AppComponent', () => {
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create the app', () => {
@@ -53,6 +61,13 @@ describe('AppComponent', () => {
     const button = fixture.nativeElement.querySelector('#button input')
     button.click();
     fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('#box')).toBeTruthy();
+  });
+
+  it('should show if box is checked3', async () => {
+    const app = fixture.componentInstance;
+    const buttons = await loader.getAllHarnesses(MatCheckboxHarness);
+    await buttons[0].check();
     expect(fixture.nativeElement.querySelector('#box')).toBeTruthy();
   });
 });
